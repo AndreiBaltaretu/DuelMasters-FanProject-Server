b30f254a71b41ab7c2dab5bc2878e005538b20c4
Andrei <AndreiBaltaretu@users.noreply.github.com>
2024-06-08 14:29:51 +0300

    Initial commit

---
+++ src/main/java/com/andreibaltaretu/duelmastersfanproject/DuelmastersfanprojectApplication.java
@@ -0,0 +1,13 @@
+package com.andreibaltaretu.duelmastersfanproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuelmastersfanprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuelmastersfanprojectApplication.class, args);
+	}
+
+}
